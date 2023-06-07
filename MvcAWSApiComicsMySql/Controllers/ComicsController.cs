f5877379d60a59e38811bc5695192adea0d4b7e2
xiangcitoZ <pzxiang1@gmail.com>
2023-06-07 10:19:33 +0200

    Add project files.

---
+++ MvcAWSApiComicsMySql/Controllers/ComicsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcAWSApiComicsMySql.Models;
+using MvcAWSApiComicsMySql.Services;
+
+namespace MvcAWSApiComicsMySql.Controllers
+{
+    public class ComicsController : Controller
+    {
+        private ServiceApiComics service;
+
+        public ComicsController(ServiceApiComics service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Comic> comics = await this.service.GetComicsAsync();
+            return View(comics);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            Comic comic = await this.service.FindComicAsync(id);
+            return View(comic);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Comic comic)
+        {
+            await this.service.CreateComic(comic);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ MvcAWSApiComicsMySql/Services/ServiceApiComics.cs
@@ -0,0 +1,79 @@
+﻿using MvcAWSApiComicsMySql.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace MvcAWSApiComicsMySql.Services
+{
+    public class ServiceApiComics
+    {
+        private MediaTypeWithQualityHeaderValue Header;
+        private string UrlApi;
+
+        public ServiceApiComics(IConfiguration configuration)
+        {
+            this.Header =
+                new MediaTypeWithQualityHeaderValue("application/json");
+            this.UrlApi =
+                configuration.GetValue<string>("ApiUrls:ApiComics");
+        }
+
+        private async Task<T> CallApiAsync<T>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                //LO UNICO QUE DEBEMOS TENER EN CUENTA ES 
+                //QUE LAS PETICIONES, A VECES SE QUEDAN ATASCADAS
+                //SI LAS HACEMOS MEDIANTE .BaseAddress + Request
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                string url = this.UrlApi + request;
+                HttpResponseMessage response =
+                    await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data = await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+
+        public async Task CreateComic(Comic comic)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/comics";
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                string jsonComic = JsonConvert.SerializeObject(comic);
+                StringContent content =
+                    new StringContent(jsonComic, Encoding.UTF8, "application/json");
+                HttpResponseMessage response =
+                    await client.PostAsync(this.UrlApi + request, content);
+            }
+        }
+
+        public async Task<List<Comic>> GetComicsAsync()
+        {
+            string request = "api/comics";
+            List<Comic> comics = await this.CallApiAsync<List<Comic>>(request);
+            return comics;
+        }
+
+        public async Task<Comic> FindComicAsync(int id)
+        {
+            string request = "api/comics/" + id;
+            Comic comic = await this.CallApiAsync<Comic>(request);
+            return comic;
+        }
+
+
+
+
+
+    }
+}
